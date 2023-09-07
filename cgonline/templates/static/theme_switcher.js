99c6c07a5fe1fb080184c4b3a0ff13fab2799f9a
Camille Gouneau <camille.gouneau@laposte.net>
2023-09-07 11:26:22 +0200

    Added footer, theme toggling

---
+++ cgonline/templates/static/theme_switcher.js
@@ -0,0 +1,37 @@
+
+function themeSwitch() {
+    const theme = localStorage.getItem('theme')
+    if (theme == 'dark') {
+        localStorage.setItem('theme', "light")
+        document.documentElement.setAttribute('data-bs-theme','light')
+        document.getElementById('iconSwitch').setAttribute('class', 'bi bi-moon-stars-fill')
+    }
+    else {
+        localStorage.setItem('theme', "dark")
+        document.documentElement.setAttribute('data-bs-theme','dark')
+        document.getElementById('iconSwitch').setAttribute('class', 'bi bi-brightness-high-fill')
+    }
+}
+
+function updateTheme() {
+    const theme = localStorage.getItem('theme')
+    if (theme == 'dark') {
+        document.documentElement.setAttribute('data-bs-theme','light')
+        document.getElementById('iconSwitch').setAttribute('class', 'bi bi-moon-stars-fill')
+        document.getElementById('navLogoLight').style.display = 'block'
+        document.getElementById('navLogoDark').style.display = 'none'
+    }
+    else {
+        document.documentElement.setAttribute('data-bs-theme','dark')
+        document.getElementById('iconSwitch').setAttribute('class', 'bi bi-brightness-high-fill')
+        document.getElementById('navLogoLight').style.display = 'none'
+        document.getElementById('navLogoDark').style.display = 'block'
+    }
+}
+
+document.getElementById('btnSwitch').addEventListener('click',()=>{
+    themeSwitch()
+    updateTheme()
+})
+
+updateTheme()
