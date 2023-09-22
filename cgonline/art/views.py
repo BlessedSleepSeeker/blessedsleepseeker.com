de22fcc26d8bf2098c87d52922bd85bf48493484
Camille Gouneau <camille.gouneau@laposte.net>
2023-09-22 18:07:25 +0200

    Main Topics Landing Page Design Unification

--- cgonline/art/views.py
+++ cgonline/art/views.py
@@ -20,7 +20,7 @@ def art_galery(request):
 
 def album(request, album_url):
     album = get_object_or_404(Album, url=album_url)
-    pieces = Piece.objects.filter(album=album)
+    pieces = Piece.objects.filter(album=album).order_by("-upload_date")
     context = {
         "album": album,
         "pieces": pieces,
@@ -31,9 +31,28 @@ def album(request, album_url):
 def piece(request, album_url, piece_url):
     album = get_object_or_404(Album, url=album_url)
     piece = get_object_or_404(Piece, url=piece_url)
+    pieces = Piece.objects.filter(album=album).order_by("-upload_date")
+    next = None
+    prev = None
+    i = 0
+    max_length = len(pieces)
+    for piece_ in pieces:
+        if piece_ == piece:
+            if i < max_length - 1:
+                next = pieces[i + 1]
+            else:
+                next = pieces[0]
+            if i > 0:
+                prev = pieces[i - 1]
+            else:
+                prev = pieces[max_length - 1]
+        i += 1
     context = {
         "album": album,
         "piece": piece,
+        "next": next,
+        "prev": prev,
+        "is_random": False if max_length > 1 else True,
     }
     return render(request, "art/generic_piece.html", context)
 
@@ -51,5 +70,6 @@ def art_random(request):
     context = {
         "piece": random.choice(Piece.objects.all()),
         "album": piece.album,
+        "is_random": True,
     }
     return render(request, "art/generic_piece.html", context)
