0c52c93a03a1f6d53ae6b4aa8321d1b154fc4145
Camille Gouneau <camille.gouneau@laposte.net>
2023-09-22 21:43:49 +0200

    Blog main search features + pagination handling in base template

--- cgonline/blog/urls.py
+++ cgonline/blog/urls.py
@@ -4,6 +4,8 @@ from . import views
 
 urlpatterns = [
     path("", views.blog_main, name="blog_main"),
+    path("tag/<slug:tag>", views.search_tags, name="tags"),
+    path("<slug:entry_url>", views.blog_entry, name="entry"),
 ]
 
 app_name = "blog"
--- cgonline/blog/views.py
+++ cgonline/blog/views.py
@@ -1,7 +1,42 @@
 from django.http import HttpResponse
-from django.shortcuts import render
+from django.shortcuts import get_object_or_404, render
+from django.core.paginator import Paginator
+
+from .models import Article
+
+ITEM_PER_PAGE = 10
 
 # Create your views here.
 def blog_main(request):
-    context = {}
+    articles = Article.objects.all().order_by("-upload_date")
+    paginator = Paginator(articles, ITEM_PER_PAGE)
+
+    page_nbr = request.GET.get("page")
+    page_obj = paginator.get_page(page_nbr)
+
+    print(page_obj.paginator.num_pages)
+    context = {
+        "page_obj": page_obj,
+    }
+    return render(request, "blog/main.html", context)
+
+
+def blog_entry(request, entry_url):
+    context = {
+        "article": get_object_or_404(Article, short_title=entry_url),
+    }
+    return render(request, "blog/main.html", context)
+
+
+def search_tags(request, tag):
+    articles = Article.objects.filter(tags__contains=tag).order_by("-upload_date")
+    paginator = Paginator(articles, ITEM_PER_PAGE)
+
+    page_nbr = request.GET.get("page")
+    page_obj = paginator.get_page(page_nbr)
+
+    context = {
+        "page_obj": page_obj,
+        "tag": tag,
+    }
     return render(request, "blog/main.html", context)
